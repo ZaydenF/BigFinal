fc4ab4d4fe63acd374d25971e459e69ad807ac85
ZaydenF <zaydenfleming@gmail.com>
2025-01-23 10:58:31 -0800

    Changed to https

--- webapp.py
+++ webapp.py
@@ -143,7 +143,7 @@ def leaderboard():
     return render_template('leaderboard.html', scores=scores)
 @app.route('/login')
 def login():   
-    return github.authorize(callback=url_for('authorized', _external=True, _scheme='http')) #callback URL must match the pre-configured callback URL
+    return github.authorize(callback=url_for('authorized', _external=True, _scheme='https')) #callback URL must match the pre-configured callback URL
 
 @app.route('/logout')
 def logout():
